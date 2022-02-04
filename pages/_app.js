7d4ab5f3bcb22fbb1d42f1a1bcbfaace0a808017
cmosley <cmosley@beholdmedia.com>
2022-02-03 21:43:21 -0600

    home page button layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@styles/globals.css'
+import '@styles/Index.module.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import styles from '@styles/Index.module.css'
 
 export default function Home() {
   return (
@@ -11,13 +12,14 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div className="container">
+          <div className={styles.sos}>SOS</div>
+          <div className={styles.options}>Locate ME</div>
+          <div className={styles.options}>CHAT</div>
+        </div>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
