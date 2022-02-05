ad130aea5aae1016a6c8015ce425db3e57fc6d58
cmosley <cmosley@beholdmedia.com>
2022-02-05 00:35:15 -0600

    text and call test setup

--- voice.js
+++ voice.js
@@ -1,13 +1,23 @@
+require("dotenv").config();
+
 const accountSid = process.env.TWILIO_ACC_SID; // Your Account SID from www.twilio.com/console
 const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
 const twilio = require("twilio");
 const client = new twilio(accountSid, authToken);
 
+client.calls
+  .create({
+    url: "http://demo.twilio.com/docs/voice.xml",
+    to: process.env.MY_PHONE_NUMBER,
+    from: process.env.TWILIO_PHONE_NUMBER,
+  })
+  .then((call) => console.log(call.sid));
+
 client.messages
   .create({
     body: "Hello from Node",
-    to: "+18179290248", // Text this number
-    from: "+18179936527", // From a valid Twilio number
+    to: process.env.MY_PHONE_NUMBER,
+    from: process.env.TWILIO_PHONE_NUMBER,
   })
   .then((message) => console.log(message.sid));
